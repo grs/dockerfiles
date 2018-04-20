e065a8cf39268a02a5875d34421f34e198620bd4
Gordon Sim <gsim@redhat.com>
2018-04-20 13:49:37 +0100

    handle auto connection assuming kubernetes statefulset

--- qpid_dispatch_router/run_qdr.sh
+++ qpid_dispatch_router/run_qdr.sh
@@ -3,4 +3,22 @@
 export HOSTNAME_IP_ADDRESS=$(hostname -i)
 
 DOLLAR='$' envsubst < /etc/qpid-dispatch/qdrouterd.conf.template > /tmp/qdrouterd.conf
+if [ -z "$DISABLE_AUTO_APPEND_CONNECTORS" ]; then
+    INDEX=$(echo "$HOSTNAME" | rev | cut -f1 -d-)
+    PREFIX=$(echo "$HOSTNAME" | rev | cut -f2- -d- | rev)
+    COUNT=0
+    while [ $COUNT -lt $INDEX ]; do
+        cat <<EOF >> /tmp/qdrouterd.conf
+connector {
+    name: $PREFIX-$COUNT
+    host: $PREFIX-$COUNT.$(hostname -d)
+    port: 55672
+    role: inter-router
+    sslProfile: inter_router_tls
+}
+EOF
+        let COUNT=COUNT+1
+    done
+fi
+
 exec /sbin/qdrouterd --conf /tmp/qdrouterd.conf
